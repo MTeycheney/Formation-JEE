2d4d4de8b11cdb9c2a730e4e7d1180d64369d080
MTeycheney <maxime.teycheney@gmail.com>
2018-12-10 18:55:34 +0100

    suppression doublon dossier TP JAVA

--- TPS JAVA/Java projects/src/ITestJava8.java
+++
@@ -1,8 +0,0 @@
-public interface ITestJava8
-{
-	default int carre(int n)
-	{
-		return n * n;
-	}
-		
-}
--- TPS JAVA/Java projects/src/ImpressionHorsLimiteException.java
+++
@@ -1,6 +0,0 @@
-public class ImpressionHorsLimiteException extends Exception
-{
-
-	private static final long serialVersionUID = -7863191838953722572L;
-
-}
